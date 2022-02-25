3b9590eca65612c2dfb96cc1c718608ee68c9367
amjadmokhu100 <amjadmokhu100@outlook.com>
2022-02-25 16:12:41 +0300

    Add project files.

---
+++ PaperHelp_Admin/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaperHelpAdmin.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ PaperHelp_Admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PaperHelp_Admin.MvcApplication" Language="C#" %>
